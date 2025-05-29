c4846c24640c6c7b7284383c962cdf3b24f6f6cf
nakulshah04 <shahnakul@gmail.com>
2025-05-29 18:03:43 +0530

    connecting to db and throwing appropriate error messages

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,12 @@
+// Importing mongoose for MongoDB connection and handling connection errors
+import mongoose from 'mongoose';
+
 export const connectDB = async () => {
     try {
-        
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Code 1 means failure (while Code 0 means success)
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ This file is the entry point for our API. */
 import dotenv from "dotenv";
 // To be able to do this, we go to package.json and create "type": "module" to use import export syntax
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -14,8 +15,9 @@ const app = express();
 
 app.get("/", (req, res) => {});
 
-console.log(process.env.MONGO_URI)
+// console.log(process.env.MONGO_URI)
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 })
