50fc22953654871687e418a306701c1a56c81dd2
nakulshah04 <shahnakul@gmail.com>
2025-05-29 17:51:27 +0530

    setting up mongodb and configuring its uri in .env

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ This file is the entry point for our API. */
 
 // const express = require('express');
 
-import express from 'express';
+// To access the MongoDB URI
+import dotenv from "dotenv";
 // To be able to do this, we go to package.json and create "type": "module" to use import export syntax
+import express from "express";
+
+dotenv.config();
 
 const app = express();
+
+app.get("/", (req, res) => {});
+
+console.log(process.env.MONGO_URI)
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
